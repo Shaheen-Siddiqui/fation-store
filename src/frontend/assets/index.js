8ba4012b79abb029a9af4ccb50a4259f1708eb4f
Md-sahil-a <mdsahilamaan2017@gmail.com>
2023-12-29 01:04:19 +0530

    update

--- src/frontend/assets/index.js
+++ src/frontend/assets/index.js
@@ -1,37 +1,38 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-import purple from "./purple-bed.jpg";
-import image1 from "./image1.jpg";
-import image2 from "./image2.jpg";
-import image3 from "./image3.webp";
+import purple from './purple-bed.jpg';
+import image1 from './image1.jpg';
+import image2 from './image2.jpg';
+import image3 from './image3.webp';
 // import image4 from './image4/avif'
-import image5 from "./image5.jpg";
-import image6 from "./image6.webp";
+import image5 from './image5.jpg';
+import image6 from './image6.webp';
 // brands
 // import godregBrand from "./godreg-brand.jpg";
 // import hometownBrand from "./hometown-brand.avif";
 // import nilKamal from "./nikamal-brand.jpg";
 // import woodenStreetBrand from "./woodenStreet-brand.avif";
-import empatyCart from './empty-cart.gif'
+import empatyCart from './empty-cart.gif';
 // import changeOfAddress from './change-of-addresses.gif'
 
 export const sliderImages = [
-    // {
-    //   url: "",
-    // },
-    {
-      url: "https://res.cloudinary.com/dunfe1kwv/image/upload/v1703787952/waistcoat/qh42nktj7zscylgy41lw.jpg",
-    },
-    // {
-    //   url: "https://smartfurniture.com.eg/wp-content/uploads/2022/06/Cover-1.png",
-    // },
-    // {
-    //   url: "https://www.rastogifurniture.in/software/uploads/slider/b68f31b350374289baf6a3b339bfc5bd.jpg",
-    // },
-    // {
-    //   url: "https://media.4rgos.it/i/Argos/0323-M020-PLP-supplier-swoon-furniture-desktab?qlt=75&fmt.jpeg.interlaced=true",
-    // },
-  ];
+  {
+    url: 'http://res.cloudinary.com/dunfe1kwv/image/upload/v1703791230/cl6srsr4t3mcvs5rcu9n.png',
+  },
+
+  {
+    url: 'http://res.cloudinary.com/dunfe1kwv/image/upload/v1703790785/oqftyanangtqxkshsqqr.png',
+  },
+  {
+    url: 'http://res.cloudinary.com/dunfe1kwv/image/upload/v1703790988/k9btflhkstqyvxnol5uz.png',
+  },
+  {
+    url: 'https://res.cloudinary.com/dunfe1kwv/image/upload/v1703787952/waistcoat/qh42nktj7zscylgy41lw.jpg',
+  },
+  {
+    url: 'http://res.cloudinary.com/dunfe1kwv/image/upload/v1703791141/itfiklw2gvp2ewpyqtgy.png',
+  },
+];
 export {
   purple,
   image1,
@@ -45,5 +46,4 @@ export {
   image6,
   empatyCart,
   // changeOfAddress,
-  
 };
